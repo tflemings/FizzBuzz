39a92b26bb63d30f590796209a916a0983eb4d22
tflemings <umadbro554@yahoo.com>
2013-09-10 07:17:19 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href='http://fonts.googleapis.com/css?family=Rokkitt' rel='stylesheet' type='text/css'>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <title>FizzBuzz</title>
+    </head>
+    <body>
+        <div id="content">
+            <h1>FizzBuzz</h1>
+            <p></p>
+            <fieldset>
+                <legend>Input Your Data</legend>
+                <form action="fizzbuzz.php" method="post">
+                    <label for=""
+                </form>
+            </fieldset> 
+        </div>
+    </body>
+</html>
