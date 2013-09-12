6956aff02febe9597228f6abb61a60bb915f5a92
tflemings <umadbro554@yahoo.com>
2013-09-12 07:18:05 -0400

    Added FIZZBUZZ Algorithm

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,4 +1,21 @@
 <?php
 define ("THREE", "Fizz");
 define ("FIVE", "Buzz");
+$start = $_POST['start'];
+$stop = $_POST['stop'];
+$output = '';
+for ($x = $start; $x <= $stop; $x++) {
+    if ($x % 3 == 0) {
+        $output = THREE;
+        if ($x % 5 == 0) {
+            $output .= FIVE;
+        }
+    } elseif ($x % 5 == 0) {
+        $output = FIVE;
+    } else {
+        $output = $x;
+    }
+    echo $output . "<br />"; 
+}
 ?>
+<a href="index.php">Back to form</a> <?php
